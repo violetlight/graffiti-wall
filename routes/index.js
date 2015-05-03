0cf9acc668e3c5e91da59827618678c5bc634c18
JF <jasonhfields@gmail.com>
2015-05-03 18:19:21 -0500

    posting to '/' creates Graffiti

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var Graffiti = require('../models/graffiti');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* write to wall */
+router.post('/', function(req, res, next) {
+  var graffiti = new Graffiti({
+    body: req.body.body
+  });
+
+  graffiti.save(function(err, data){
+    if (err) console.log(err);
+    else console.log('saved: ', data);
+    res.render('index');
+  });
+});
+
 
 module.exports = router;
