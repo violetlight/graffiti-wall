5c91d5e33975bbd510b812f5d6079f53866482a8
JF <jasonhfields@gmail.com>
2015-05-04 12:59:05 -0500

    Deletes obj from list when its age > ageLimit

--- public/js/lib/wall.js
+++ public/js/lib/wall.js
@@ -3,17 +3,25 @@ var canvas = document.getElementById('c'),
     w = canvas.width = window.innerWidth/1.5,
     h = canvas.height = window.innerHeight/1.5,
     start = null,
+    second = 1000, // milliseconds
     frame = 0,
     allGraffiti = [],
+    ageLimit = 3*(second*60), // three minutes
     background = tinycolor('#232323');
 
 
+function rangeMap(inMin, inMax, outMin, outMax, inVal) {
+  // this function maps a value in the range
+  //    inMin < inVal < inMax
+  //  in proportion to a value in the range
+  //    outMin < outVal < outMax
+  // and returns outVal
+  var outVal = ((inVal - inMin) * (outMax - outMin) / (inMax - inMin)) + outMin;
+  return outVal;
+}
+
 function Graffiti() {
-  this.body = ' ';
   this.font = '48px serif';
-  this.x = Math.random() * w;
-  this.y = Math.random() * h;
-  this.color = tinycolor.random();
 }
 
 Graffiti.prototype.update = function() {
@@ -31,16 +39,29 @@ function update(timestep) {
 
   for (var i=0; i<allGraffiti.length; i++) {
     allGraffiti[i].update();
+    console.log(allGraffiti[i].age);
+    console.log(ageLimit);
+    if (allGraffiti[i].age > ageLimit) {
+      allGraffiti.splice(i, 1);
+      i--;
+    }
   }
 }
 
 
 function initLoop(g) { // pass graffitiData in
+  var now = new Date();
 
-  //instantiate objects based on data
+  // unpack data and instantiate objets for canvas
   for (var i=0; i<g.length; i++) {
     var obj = new Graffiti;
+    obj.age = (now - new Date(g[i].createdAt));
+    console.log(obj.age);
     obj.body = g[i].body;
+    var txt = ctx.measureText(g[i].body);
+    obj.x = rangeMap(0, 100, 0, w-txt.width*2, g[i].x);
+    obj.y = rangeMap(0, 100, 0, w, g[i].y);
+    obj.color = tinycolor(g[i].color);
     allGraffiti.push(obj);
   }
 
