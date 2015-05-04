99d748a074dfa6f99c63a9fa50f3d5ec7bbea7f3
JF <jasonhfields@gmail.com>
2015-05-04 12:58:28 -0500

    initialize values in schema

--- models/graffiti.js
+++ models/graffiti.js
@@ -1,8 +1,22 @@
 var mongoose = require('mongoose');
 
 var graffitiSchema = mongoose.Schema({
+
   body: String,
-  createdAt: { type: Date, default: Date.now }
+
+  createdAt: { type: Date, default: Date.now },
+
+  x: { type: Number, default: function () {
+    return Math.random() * 100;
+  }},
+  y: { type: Number, default: function () {
+    return Math.random() * 100;
+  }},
+
+  color: { type: String, default: function() {  // somewhat random hex color
+    return (Math.floor(Math.random() * (0xFFFFFF - 0x010000)+0x010000)).toString(16);
+  }}
+
 });
 
 module.exports = mongoose.model('Graffiti', graffitiSchema);
