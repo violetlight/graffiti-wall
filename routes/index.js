413fbebc7e57811748e7517239a563e6732deb88
JF <jasonhfields@gmail.com>
2015-05-04 14:16:06 -0500

    Fix title and don't pass it into the view

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var Graffiti = require('../models/graffiti');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 /* write to wall */
